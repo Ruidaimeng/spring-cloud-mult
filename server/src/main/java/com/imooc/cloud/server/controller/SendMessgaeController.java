dbd7f6c1744e0cb1367787dc6eaf776942c26787
Ruidaimeng <13816570705@163.com>
2018-10-08 20:49:04 +0800

    加入stream，使用消息队列

---
+++ server/src/main/java/com/imooc/cloud/server/controller/SendMessgaeController.java
@@ -0,0 +1,32 @@
+package com.imooc.cloud.server.controller;
+
+import com.imooc.cloud.server.message.StreamClient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+
+/**
+ * 消息发送
+ *
+ * @author ruimeng
+ * @create 2018-10-08 19:53
+ **/
+@RestController
+public class SendMessgaeController {
+
+    @Autowired
+    private StreamClient streamClient ;
+
+    @GetMapping("/sendMessage")
+    public void process(){
+        streamClient.output().send(
+                MessageBuilder.withPayload("now"+new Date()).build()
+        );
+    }
+
+}
