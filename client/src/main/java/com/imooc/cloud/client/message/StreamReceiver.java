0ab913a6dc6fc35cc306aef052ce6e54a63f3f33
Ruidaimeng <13816570705@163.com>
2018-10-08 20:48:16 +0800

    加入stream，使用消息队列

---
+++ client/src/main/java/com/imooc/cloud/client/message/StreamReceiver.java
@@ -0,0 +1,22 @@
+package com.imooc.cloud.client.message;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 使用stream获取消息
+ *
+ * @author ruimeng
+ * @create 2018-10-08 19:47
+ **/
+@Component
+@EnableBinding(StreamClient.class)
+public class StreamReceiver {
+
+    //消息接受端
+    @StreamListener("myMessage")
+    public void process(Object message){
+        System.out.println("myMessage:"+message);
+    }
+}
